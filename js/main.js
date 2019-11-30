a37c6f1b16a4d081b8892b7b7be1c13531faae5f
kemeh <knm1988@gmail.com>
2019-11-30 16:45:46 +0200

    status (online/offline) added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,57 @@
-window.onload = () => {
-    'use strict';
-  
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-               .register('./sw.js');
-    }
+const handleConnection = () => {
+  if (navigator.onLine) {
+    isReachable(getServerUrl()).then((online) => {
+      if (online) {
+        // handle online status
+        setOnlineStatus(true);
+      } else {
+        setOnlineStatus(false);
+      }
+    });
+  } else {
+    // handle offline status
+    setOnlineStatus(false);
   }
+}
+
+const isReachable = (url) => {
+  return fetch(url, { method: 'HEAD', mode: 'no-cors' })
+    .then((resp) => {
+      return resp && (resp.ok || resp.type === 'opaque');
+    })
+    .catch((err) => {
+      console.warn('[conn test failure]:', err);
+    });
+}
+
+const getServerUrl = () => window.location.origin;
+
+const registerServiceWorker = () => {
+  'use strict';
+  
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./sw.js')
+      .then((workerRegistration) => {
+        handleConnection();
+      });
+  }
+}
+
+const setOnlineStatus = (isOnline) => {
+  const offlineClass = 'offline';
+  const onlineClass = 'online';
+  let element = document.querySelector('.status');
+
+  if (isOnline){
+    element.classList.add(onlineClass);
+    element.classList.remove(offlineClass);
+  } else {
+    element.classList.remove(onlineClass);
+    element.classList.add(offlineClass);
+  }
+}
+
+window.onload = registerServiceWorker;
+window.ononline = handleConnection;
+window.onoffline = handleConnection;  
--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@ var cacheName = 'hello-pwa';
 var filesToCache = [
     '/',
     '/index.html',
+    '/manifest.json',
+    '/favicon.ico',
     '/css/style.css',
     '/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener('install', (e) => {
     e.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-        return cache.addAll(filesToCache);
-      })
+      caches.open(cacheName)
+            .then((cache) => cache.addAll(filesToCache))
     );
   });
 
   /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', (e) => {
     e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
+      caches.match(e.request)
+            .then((response) => response || fetch(e.request))
     );
   });
